e4777e8cab504a489634479a43621630df78fee4
norhan taha21 <norhantaha43@gmail.com>
2026-08-07 23:27:29 +0300

    Update ShapeRenderingSystem

---
+++ ShapeRenderingSystem/ShapeRenderingSystem/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeRenderingSystem
+{
+    public class Triangle : Shape, IDrawable
+    {
+        public double Base {  get; set; }
+        public double Height { get; set; }
+        public override double Area()
+        {
+            return 0.5 * Base * Height;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("    *");
+            Console.WriteLine("  ***");
+            Console.WriteLine(" *****");
+            Console.WriteLine($"Area = {Area()}");
+        }
+    }
+}
