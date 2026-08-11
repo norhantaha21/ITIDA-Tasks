d0d152b0a92fe2374a3fde1ccc13e81cdfcccc32
norhan taha21 <norhantaha43@gmail.com>
2026-08-11 23:28:39 +0300

    Revert "Add ProductsController task"
    This reverts commit 48317ab95491c72ffdd30c4ad8df04e0574cf56b.

---
+++ BankAccount/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcc account1= new BankAcc();
+            account1.owner = "Ali";
+            account1.Deposit(1000);
+
+            SavingsAccount account2= new SavingsAccount();
+            account2.owner = "Sara";
+            account2.InterestRate = 10;
+            account2.Deposit(2000);
+            account2.ApplyInterest();
+
+            BankAcc[] accounts = [account1, account2];
+
+            foreach (BankAcc acc in accounts) {
+                Console.WriteLine($"Type:{acc.GetAccountType()}");
+                Console.WriteLine($"Balance:{acc.Balance}");
+            }
+        }
+    }
+}
--- ProductsController/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductsController.Models
-{
-    public class Product
-    {
-            public int Id { get; set; }
-            public string Name { get; set; } = string.Empty;
-            public decimal Price { get; set; }
-    }
-}
